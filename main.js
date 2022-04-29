77dce6f96f5ff60dacde558d5f5c7634bbc91d50
K.Tej Vetrivelan <97396129+Tejvetrivelan@users.noreply.github.com>
2022-04-29 15:22:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,64 +1,86 @@
-    //Create "mouseEvent" variable and set it as “Empty”.
 var mouseEvent = "empty";
-
-    //Create two variables "last_position_of_x" and  "last_position_of_y".
-    var last_position_of_x, last_position_of_y;
 
     canvas = document.getElementById('myCanvas');
     ctx = canvas.getContext("2d");
     
-    //Define two variables color and width_of_line and assign values.
-    color = "Dark green";
-    width_of_line = 6;
+    color = "black";
+    width_of_line = 2;
 
     canvas.addEventListener("mousedown", my_mousedown);
 
     function my_mousedown(e)
     {
-        //Addictonal Activity start
-        
-        //Addictonal Activity ends
-
-        mouseEvent = "mousedown";
+        color = document.getElementById("color").value;
+        width_of_line = document.getElementById("width_of_line").value;
+        radius = document.getElementById("radius").value;
+        mouseEvent = "mouseDown";
     }
 
-    //Create the addEventListener() function for mouseup(). 
-canvas.addEventListener("mouseup", my_mouseup);
-function my_mouseup(e)
-{
-    mouseEvent = "mouseUP";
-}
-    //Create the addEventListener() function for mouseleave(). 
+    canvas.addEventListener("mousemove", my_mousemove);
 
-
-
-
-canvas.addEventListener("mousemove", my_mousemove);
-   //Create the my_mouseup() function. Assign “mouseUP” in the variable mouseEvent.
     function my_mousemove(e)
     {
+
         current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
         current_position_of_mouse_y = e.clientY - canvas.offsetTop;
 
-        //Check whether the mouseEvent is "mousedown".  
         if (mouseEvent == "mouseDown") {
-            ctx.bigenPath();
-            ctx.strokeStyle = color;
-            ctx.lineWidth = width_of_line;
-
-            console.log("last position of x and y coordinates = ");
-            console.log("x = " + last_position_of_x + "y = " + last_position_of_y);
-            ctx.moveTo(last_position_of_x, last_position_of_y);
-
-            console.log("Current position of x and y coordinates = ");
-            console.log("x = " + current_position_of_mouse_x + current_position_of_mouse_y);
-            ctx.lineTo(current_position_of_mouse_x, current_position_of_mouse_y);
-            ctx.stroke();
+        console.log("Current position of x and y coordinates = ");
+        console.log("x  = " + current_position_of_mouse_x + "y = " + current_position_of_mouse_y);
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+        ctx.lineWidth = width_of_line;
+        ctx.arc(current_position_of_mouse_x, current_position_of_mouse_y, radius ,0 , 2 * Math.PI);
+        ctx.stroke();
         }
 
+    }
 
+    canvas.addEventListener("mouseup", my_mouseup);
 
-        last_position_of_x = current_position_of_mouse_x; 
-        last_position_of_y = current_position_of_mouse_y;
+    function my_mouseup(e)
+    {
+
+        current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
+        current_position_of_mouse_x = e.clientX - canvas.offsetRight;
+        current_position_of_mouse_x = e.clientX - canvas.offsetBottom;
+
+        current_position_of_mouse_y = e.clientY - canvas.offsetTop;
+
+        if (mouseEvent == "mouseup") {
+        console.log("Current position of x and y coordinates = ");
+        console.log("x  = " + current_position_of_mouse_x + "y = " + current_position_of_mouse_y);
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+        ctx.lineWidth = width_of_line;
+        ctx.arc(current_position_of_mouse_x, current_position_of_mouse_y, radius ,0 , 2 * Math.PI);
+        ctx.stroke();
+        }
 
     }
+
+    function my_mouseleave(e)
+    {
+
+        current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
+        current_position_of_mouse_x = e.clientX - canvas.offsetRight;
+        current_position_of_mouse_x = e.clientX - canvas.offsetBottom;
+
+        current_position_of_mouse_y = e.clientY - canvas.offsetTop;
+
+        if (mouseEvent == "mouseleave") {
+        console.log("Current position of x and y coordinates = ");
+        console.log("x  = " + current_position_of_mouse_x + "y = " + current_position_of_mouse_y);
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+        ctx.lineWidth = width_of_line;
+        ctx.arc(current_position_of_mouse_x, current_position_of_mouse_y, radius ,0 , 2 * Math.PI);
+        ctx.stroke();
+        }
+
+    }
+
+
+function clearArea() {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
